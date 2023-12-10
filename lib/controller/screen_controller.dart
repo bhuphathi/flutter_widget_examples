adf84d8c4f0de60238da0d917f1682e06a0f4ed7
bhuphathi <bhuphathi@gmail.com>
2023-12-10 12:37:26 +0530

    feat: get screen method added in screencontroller fix: list of args to map

--- lib/controller/screen_controller.dart
+++ lib/controller/screen_controller.dart
@@ -1,4 +1,9 @@
 import 'package:flutter_widget_examples/screens.dart';
+import 'package:flutter_widget_examples/screens/card_expansion.dart';
+import 'package:flutter_widget_examples/screens/excel_file_view.dart';
+import 'package:flutter_widget_examples/screens/overlay_portal.dart';
+import 'package:flutter_widget_examples/screens/staggered_gridtile_count.dart';
+import 'package:flutter_widget_examples/screens/staggered_masonary_grid.dart';
 import 'package:flutter_widget_examples/temp/fluent_icons.dart';
 import 'package:flutter_widget_examples/temp/font_awesome_gallery.dart';
 import 'package:flutter_widget_examples/temp/gradient_bg_screen.dart';
@@ -22,6 +27,11 @@ enum Screens {
   sidePanelExample,
   testScreenDemo,
   screens,
+  excelFile,
+  cardExpansionDemo,
+  overlayPortalDemo,
+  staggeredMasonaryGrid,
+  staggeredGridTileCount,
 }
 
 /// Home() screen is default
@@ -32,7 +42,7 @@ class ScreenNotifier extends StateNotifier<ScreenController> {
   Ref ref;
   ScreenNotifier(super.state, this.ref);
 
-  void switchScreen({required Screens screen, List? args}) {
+  void switchScreen({required Screens screen, Map<String, dynamic>? args}) {
     state = ScreenController(ref: ref, screenName: screen, args: args);
   }
 }
@@ -41,12 +51,12 @@ class ScreenController {
   late Widget _currentScreen;
   final Ref ref;
   final Screens _screenName;
-  final List? _args;
+  final Map<String, dynamic>? _args;
 
   ScreenController({
     required this.ref,
     Screens? screenName = Screens.home,
-    List? args,
+    Map<String, dynamic>? args,
   })  : _screenName = screenName!,
         _args = args {
     _switchScreen();
@@ -88,9 +98,61 @@ class ScreenController {
       case Screens.screens:
         _currentScreen = const ScreensDemo();
         break;
+      case Screens.excelFile:
+        _currentScreen = const ExcelFileDemo();
+        break;
+      case Screens.cardExpansionDemo:
+        _currentScreen = const CardExpansionDemo();
+        break;
+      case Screens.overlayPortalDemo:
+        _currentScreen = const OverlayPortalDemo();
+        break;
+      case Screens.staggeredMasonaryGrid:
+        _currentScreen = const StaggeredMasonaryGrid();
+        break;
+      case Screens.staggeredGridTileCount:
+        _currentScreen = const StaggeredGridTileCount();
+        break;
       default:
         _currentScreen = const Home();
         break;
+    }
+  }
+  static Widget getScreen(Screens screen) {
+    switch (screen) {
+      case Screens.fluentUIShowcaseWidgetDemo:
+        return const FluentUIShowcaseWidget();
+      case Screens.fontAwesomeGalleryDemo:
+        return const FontAwesomeGallery();
+      case Screens.gradientBgDemo:
+        return const GradientBg();
+      case Screens.home:
+        return const Home();
+      case Screens.lifeCycleDemo:
+        return const Lifecycle();
+      case Screens.radixTestScreenDemo:
+        return const RadixTestScreen();
+      case Screens.readWriteDemo:
+        // assert(_args?[0] != null, "CounterStorage() class need as args[0] for ReadWriteDemo screen");
+        return ReadWriteDemo(storage: CounterStorage());
+      case Screens.sidePanelExample:
+        return const SidePanelExample();
+      case Screens.testScreenDemo:
+        return const TestScreen();
+      case Screens.screens:
+        return const ScreensDemo();
+      case Screens.excelFile:
+        return const ExcelFileDemo();
+      case Screens.cardExpansionDemo:
+        return const CardExpansionDemo();
+      case Screens.overlayPortalDemo:
+        return const OverlayPortalDemo();
+      case Screens.staggeredMasonaryGrid:
+        return const StaggeredMasonaryGrid();
+      case Screens.staggeredGridTileCount:
+        return const StaggeredGridTileCount();
+      default:
+        return const Home();
     }
   }
 }
