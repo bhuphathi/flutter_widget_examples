228b697a1020765ed6d7c4317dad5c526e9c5091
bhuphathi <bhuphathi@gmail.com>
2023-12-24 00:17:26 +0530

    feat: color tile added in material theme screen

--- lib/screens/material_theme.dart
+++ lib/screens/material_theme.dart
@@ -23,7 +23,7 @@ class MaterialThemeDemo extends ConsumerWidget {
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: responsive(
                     context: context,
-                    desktopLarge: 8,
+                    desktopLarge: 6,
                     desktop: 6,
                     tabletLandscape: 4,
                     mobileLandscape: 3,
@@ -34,175 +34,104 @@ class MaterialThemeDemo extends ConsumerWidget {
                 crossAxisSpacing: 4,
                 mainAxisSpacing: 2),
             children: [
-              ListTile(
-                title: SelectableText(
-                    "background ${getColorHexValue(Theme.of(context).colorScheme.background)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onBackground)),
-                tileColor: Theme.of(context).colorScheme.background,
+              colorTile(
+                color: (color: Theme.of(context).colorScheme.background, name: "background"),
+                textOrOnColor: (color: Theme.of(context).colorScheme.onBackground, name: "onBackground"),
               ),
-              ListTile(
-                title: SelectableText(
-                    "onBackground ${getColorHexValue(Theme.of(context).colorScheme.onBackground)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.background)),
-                tileColor: Theme.of(context).colorScheme.onBackground,
+              colorTile(
+                color: (color: Theme.of(context).colorScheme.primary, name: "primary"),
+                textOrOnColor: (color: Theme.of(context).colorScheme.onPrimary, name: "onPrimary"),
               ),
-              ListTile(
-                title: SelectableText("error ${getColorHexValue(Theme.of(context).colorScheme.error)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onError)),
-                tileColor: Theme.of(context).colorScheme.error,
+              colorTile(
+                color: (color: Theme.of(context).colorScheme.inversePrimary, name: "inversePrimary"),
+                textOrOnColor: (color: Theme.of(context).colorScheme.primary, name: "primary"),
               ),
-              ListTile(
-                title: SelectableText(
-                    "onError ${getColorHexValue(Theme.of(context).colorScheme.onError)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.error)),
-                tileColor: Theme.of(context).colorScheme.onError,
+              colorTile(
+                color: (color: Theme.of(context).colorScheme.primaryContainer, name: "primaryContainer"),
+                textOrOnColor: (
+                  color: Theme.of(context).colorScheme.onPrimaryContainer,
+                  name: "onPrimaryContainer"
+                ),
               ),
-              ListTile(
-                title: SelectableText(
-                    "errorContainer ${getColorHexValue(Theme.of(context).colorScheme.errorContainer)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onErrorContainer)),
-                tileColor: Theme.of(context).colorScheme.errorContainer,
+              colorTile(
+                color: (color: Theme.of(context).colorScheme.secondary, name: "secondary"),
+                textOrOnColor: (color: Theme.of(context).colorScheme.onSecondary, name: "onSecondary"),
               ),
-              ListTile(
-                title: SelectableText(
-                    "onErrorContainer ${getColorHexValue(Theme.of(context).colorScheme.onErrorContainer)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.errorContainer)),
-                tileColor: Theme.of(context).colorScheme.onErrorContainer,
+              colorTile(
+                color: (
+                  color: Theme.of(context).colorScheme.secondaryContainer,
+                  name: "secondaryContainer"
+                ),
+                textOrOnColor: (
+                  color: Theme.of(context).colorScheme.onSecondaryContainer,
+                  name: "onSecondaryContainer"
+                ),
               ),
-              ListTile(
-                title: SelectableText(
-                    "inversePrimary ${getColorHexValue(Theme.of(context).colorScheme.inversePrimary)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.primary)),
-                tileColor: Theme.of(context).colorScheme.inversePrimary,
+              colorTile(
+                color: (color: Theme.of(context).colorScheme.error, name: "error"),
+                textOrOnColor: (color: Theme.of(context).colorScheme.onError, name: "onError"),
               ),
-              ListTile(
-                title: SelectableText(
-                    "inverseSurface ${getColorHexValue(Theme.of(context).colorScheme.inverseSurface)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onInverseSurface)),
-                tileColor: Theme.of(context).colorScheme.inverseSurface,
+              colorTile(
+                color: (color: Theme.of(context).colorScheme.errorContainer, name: "errorContainer"),
+                textOrOnColor: (
+                  color: Theme.of(context).colorScheme.onErrorContainer,
+                  name: "onErrorContainer"
+                ),
               ),
-              ListTile(
-                title: SelectableText(
-                    "onInverseSurface ${getColorHexValue(Theme.of(context).colorScheme.onInverseSurface)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.inverseSurface)),
-                tileColor: Theme.of(context).colorScheme.onInverseSurface,
+              colorTile(
+                color: (color: Theme.of(context).colorScheme.tertiary, name: "tertiary"),
+                textOrOnColor: (color: Theme.of(context).colorScheme.onTertiary, name: "onTertiary"),
               ),
-              ListTile(
-                title: SelectableText(
-                    "outline ${getColorHexValue(Theme.of(context).colorScheme.outline)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.outlineVariant)),
-                tileColor: Theme.of(context).colorScheme.outline,
+              colorTile(
+                color: (
+                  color: Theme.of(context).colorScheme.tertiaryContainer,
+                  name: "tertiaryContainer"
+                ),
+                textOrOnColor: (
+                  color: Theme.of(context).colorScheme.onTertiaryContainer,
+                  name: "onTertiaryContainer"
+                ),
               ),
-              ListTile(
-                title: SelectableText(
-                    "outlineVariant ${getColorHexValue(Theme.of(context).colorScheme.outlineVariant)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.outline)),
-                tileColor: Theme.of(context).colorScheme.outlineVariant,
+              colorTile(
+                color: (color: Theme.of(context).colorScheme.surface, name: "surface"),
+                textOrOnColor: (color: Theme.of(context).colorScheme.onSurface, name: "onSurface"),
               ),
-              ListTile(
-                title: SelectableText(
-                    "primary ${getColorHexValue(Theme.of(context).colorScheme.primary)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
-                tileColor: Theme.of(context).colorScheme.primary,
+              colorTile(color: (
+                color: Theme.of(context).colorScheme.inverseSurface,
+                name: "inverseSurface"
+              ), textOrOnColor: (
+                color: Theme.of(context).colorScheme.onInverseSurface,
+                name: "onInverseSurface"
+              )),
+              colorTile(color: (
+                color: Theme.of(context).colorScheme.surfaceVariant,
+                name: "surfaceVariant"
+              ), textOrOnColor: (
+                color: Theme.of(context).colorScheme.onSurfaceVariant,
+                name: "onSurfaceVariant"
+              )),
+              colorTile(color: (
+                color: Theme.of(context).colorScheme.outline,
+                name: "outline"
+              ), textOrOnColor: (
+                color: Theme.of(context).colorScheme.outlineVariant,
+                name: "outlineVariant"
+              )),
+              colorTile(
+                color: (color: Theme.of(context).colorScheme.outlineVariant, name: "outlineVariant"),
+                textOrOnColor: (color: Theme.of(context).colorScheme.outline, name: "outline"),
               ),
-              ListTile(
-                title: SelectableText(
-                    "onPrimary ${getColorHexValue(Theme.of(context).colorScheme.onPrimary)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.primary)),
-                tileColor: Theme.of(context).colorScheme.onPrimary,
+              colorTile(
+                color: (color: Theme.of(context).colorScheme.scrim, name: "scrim"),
+                textOrOnColor: (color: Theme.of(context).colorScheme.background, name: "background"),
               ),
-              ListTile(
-                title: SelectableText(
-                    "primaryContainer ${getColorHexValue(Theme.of(context).colorScheme.primaryContainer)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onPrimaryContainer)),
-                tileColor: Theme.of(context).colorScheme.primaryContainer,
+              colorTile(
+                color: (color: Theme.of(context).colorScheme.shadow, name: "shadow"),
+                textOrOnColor: (color: Theme.of(context).colorScheme.background, name: "background"),
               ),
-              ListTile(
-                title: SelectableText(
-                    "onPrimaryContainer ${getColorHexValue(Theme.of(context).colorScheme.onPrimaryContainer)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.primaryContainer)),
-                tileColor: Theme.of(context).colorScheme.onPrimaryContainer,
-              ),
-              ListTile(
-                title: SelectableText("scrim ${getColorHexValue(Theme.of(context).colorScheme.scrim)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.background)),
-                tileColor: Theme.of(context).colorScheme.scrim,
-              ),
-              ListTile(
-                title: SelectableText(
-                    "Secondary ${getColorHexValue(Theme.of(context).colorScheme.secondary)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onSecondary)),
-                tileColor: Theme.of(context).colorScheme.secondary,
-              ),
-              ListTile(
-                title: SelectableText(
-                    "onSecondary ${getColorHexValue(Theme.of(context).colorScheme.onSecondary)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.secondary)),
-                tileColor: Theme.of(context).colorScheme.onSecondary,
-              ),
-              ListTile(
-                title: SelectableText(
-                    "secondaryContainer ${getColorHexValue(Theme.of(context).colorScheme.secondaryContainer)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onSecondaryContainer)),
-                tileColor: Theme.of(context).colorScheme.secondaryContainer,
-              ),
-              ListTile(
-                title: SelectableText(
-                    "onSecondaryContainer ${getColorHexValue(Theme.of(context).colorScheme.onSecondaryContainer)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.secondaryContainer)),
-                tileColor: Theme.of(context).colorScheme.onSecondaryContainer,
-              ),
-              ListTile(
-                title: SelectableText("shadow ${getColorHexValue(Theme.of(context).colorScheme.shadow)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.background)),
-                tileColor: Theme.of(context).colorScheme.shadow,
-              ),
-              ListTile(
-                title: SelectableText(
-                    "surface ${getColorHexValue(Theme.of(context).colorScheme.surface)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onSurface)),
-                tileColor: Theme.of(context).colorScheme.surface,
-              ),
-              ListTile(
-                title: SelectableText(
-                    "surfaceTint ${getColorHexValue(Theme.of(context).colorScheme.surfaceTint)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
-                tileColor: Theme.of(context).colorScheme.surfaceTint,
-              ),
-              ListTile(
-                title: SelectableText(
-                    "surfaceVariant ${getColorHexValue(Theme.of(context).colorScheme.surfaceVariant)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onSurfaceVariant)),
-                tileColor: Theme.of(context).colorScheme.surfaceVariant,
-              ),
-              ListTile(
-                title: SelectableText(
-                    "tertiary ${getColorHexValue(Theme.of(context).colorScheme.tertiary)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onTertiary)),
-                tileColor: Theme.of(context).colorScheme.tertiary,
-              ),
-              ListTile(
-                title: SelectableText(
-                    "onTertiary ${getColorHexValue(Theme.of(context).colorScheme.onTertiary)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.tertiary)),
-                tileColor: Theme.of(context).colorScheme.onTertiary,
-              ),
-              ListTile(
-                title: SelectableText(
-                    "tertiaryContainer ${getColorHexValue(Theme.of(context).colorScheme.tertiaryContainer)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onTertiaryContainer)),
-                tileColor: Theme.of(context).colorScheme.tertiaryContainer,
-              ),
-              ListTile(
-                title: SelectableText(
-                    "onTertiaryContainer ${getColorHexValue(Theme.of(context).colorScheme.onTertiaryContainer)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.tertiaryContainer)),
-                tileColor: Theme.of(context).colorScheme.onTertiaryContainer,
-              ),
-              ListTile(
-                title: SelectableText("dummy ${getColorHexValue(Theme.of(context).colorScheme.surface)}",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onSurface)),
-                tileColor: Theme.of(context).colorScheme.surface,
+              colorTile(
+                color: (color: Theme.of(context).colorScheme.surfaceTint, name: "surfaceTint"),
+                textOrOnColor: (color: Theme.of(context).colorScheme.onPrimary, name: "onPrimary"),
               ),
               ListTile(
                 title: SelectableText("dummy ${getColorHexValue(Theme.of(context).colorScheme.surface)}",
@@ -213,6 +142,133 @@ class MaterialThemeDemo extends ConsumerWidget {
           ),
         ),
       ],
+    );
+  }
+
+  Widget colorTile({
+    required ({Color color, String name}) color,
+    required ({Color color, String name}) textOrOnColor,
+    ({Color color, String name})? onColor,
+    ({Color color, String name})? colorA,
+  }) {
+    if (colorA != null) {
+      return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
+        return Stack(children: [
+          Container(color: color.color),
+          Padding(
+            padding: const EdgeInsets.only(top: 2, left: 2),
+            child: colorTexts(color, textOrOnColor, onColor, colorA),
+          ),
+          Positioned.directional(
+            end: 0,
+            top: 0,
+            bottom: 0,
+            textDirection: TextDirection.ltr,
+            child: Container(
+              color: colorA.color,
+              width: constraints.maxWidth * 0.35,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SelectableText(
+                  "${getColorHexValue(colorA.color)}:\n${colorA.name}",
+                  style: TextStyle(color: textOrOnColor.color, fontSize: 12),
+                ),
+              ),
+            ),
+          ),
+        ]);
+      });
+    }
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 4),
+      tileColor: color.color,
+      title: colorTexts(color, textOrOnColor, onColor, null),
+    );
+  }
+
+  Widget colorTexts(
+    ({Color color, String name}) color,
+    ({Color color, String name}) textOrOnColor,
+    ({Color color, String name})? onColor,
+    ({Color color, String name})? colorA,
+  ) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: 16,
+                height: 16,
+                child: Card(
+                  color: color.color,
+                  shape: const RoundedRectangleBorder(),
+                  margin: EdgeInsets.zero,
+                ),
+              ),
+              const SizedBox(width: 6),
+              SelectableText("${getColorHexValue(color.color)} : ${color.name}",
+                  style: TextStyle(color: textOrOnColor.color)),
+            ],
+          ),
+          const SizedBox(height: 4),
+          Row(
+            children: [
+              SizedBox(
+                width: 16,
+                height: 16,
+                child: Card(
+                  color: textOrOnColor.color,
+                  shape: const RoundedRectangleBorder(),
+                  margin: EdgeInsets.zero,
+                ),
+              ),
+              const SizedBox(width: 6),
+              SelectableText("${getColorHexValue(textOrOnColor.color)} : ${textOrOnColor.name}",
+                  style: TextStyle(color: textOrOnColor.color)),
+            ],
+          ),
+          const SizedBox(height: 4),
+          if (onColor != null)
+            Row(
+              children: [
+                SizedBox(
+                  width: 16,
+                  height: 16,
+                  child: Card(
+                    color: onColor.color,
+                    shape: const RoundedRectangleBorder(),
+                    margin: EdgeInsets.zero,
+                  ),
+                ),
+                const SizedBox(width: 6),
+                SelectableText("${getColorHexValue(onColor.color)} : ${onColor.name}",
+                    style: TextStyle(color: onColor.color)),
+              ],
+            ),
+          const SizedBox(height: 4),
+          if (colorA != null)
+            Row(
+              children: [
+                SizedBox(
+                  width: 16,
+                  height: 16,
+                  child: Card(
+                    color: colorA.color,
+                    shape: const RoundedRectangleBorder(),
+                    margin: EdgeInsets.zero,
+                  ),
+                ),
+                const SizedBox(width: 6),
+                SelectableText("${getColorHexValue(colorA.color)} : ${colorA.name}",
+                    style: TextStyle(color: colorA.color)),
+              ],
+            ),
+        ],
+      ),
     );
   }
 }
