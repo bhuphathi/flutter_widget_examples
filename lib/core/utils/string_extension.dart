a19f62248150d7a83ccbcbe0fe028d90ea11c24a
bhuphathi <bhuphathi@gmail.com>
2024-01-01 23:21:00 +0530

    feat: string extension functions added - split camelcase, tocamelcase and remove specialcharacters

--- lib/core/utils/string_extension.dart
+++ lib/core/utils/string_extension.dart
@@ -5,4 +5,23 @@ extension StringCasingExtension on String {
   ///capitalize first character of all words in this sentence: 'This is a title' => 'This Is A Title'
   String toTitleCase() =>
       replaceAll(RegExp(' +'), ' ').split(' ').map((str) => str.toCapitalize()).join(' ');
+
+  ///remove all special characters in the list
+  String removeSpecialCharaters(List<String> charsToRemove) {
+    //add escape to all special characters
+    var specialCharacters = charsToRemove.map((e) => "\\$e").toList();
+    var regexPattern = specialCharacters.join("|");
+    return replaceAll(RegExp(regexPattern), '');
+  }
+
+  ///split at capitial letters
+  ///'thisIsACamelCaseString' => 'This Is A Camel Case String'
+  String splitCamelCase() {
+    return split(RegExp(r'(?=[A-Z])')).map((e) => e.toCapitalize()).join(' ');
+  }
+
+  String toCamelCase() {
+    var string = length > 0 ? '${this[0].toLowerCase()}${substring(1)}' : '';
+    return string.replaceAll(RegExp(r"\s+"), "");
+  }
 }
