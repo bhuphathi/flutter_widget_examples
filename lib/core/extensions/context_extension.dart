a958ffbe89d6a97b79b86d710e5d8d14e9ac82e8
bhuphathi <bhuphathi@gmail.com>
2024-03-18 12:46:44 +0530

    feat: context extensions added

---
+++ lib/core/extensions/context_extension.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widget_examples/core/utils/debug_print.dart';
+
+const _filename = "globalbounds";
+
+extension GlobalPaintBounds on BuildContext {
+  Rect? get globalPaintBounds {
+    final renderObject = findRenderObject();
+    dPrint(filename: _filename, msg: "renderObject $renderObject");
+    final translation = renderObject?.getTransformTo(null).getTranslation();
+    dPrint(filename: _filename, msg: "translation $translation");
+
+    if (translation != null && renderObject?.paintBounds != null) {
+      final offset = Offset(translation.x, translation.y);
+      dPrint(filename: _filename, msg: "offset $offset");
+      return renderObject!.paintBounds.shift(offset);
+    } else {
+      return null;
+    }
+  }
+
+  Offset getWidgetOffset() {
+    final RenderBox box = findRenderObject() as RenderBox;
+    final position = box.localToGlobal(Offset.zero);
+
+    final bottomLeftOffset = Offset(position.dx, position.dy + box.size.height);
+    // final bottomRightOffset = Offset(position.dx + box.size.width, position.dy + box.size.height);
+    return bottomLeftOffset;
+  }
+}
